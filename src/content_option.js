87b5e125646d1a0e5b16bd493d21ebe3d7c9c548
nidabatool <nida711711@gamil.com>
2024-10-24 03:36:07 -0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,140 @@
+import { FaInstagram } from "react-icons/fa";
+
+const logotext = "Nida";
+const meta = {
+    title: "Nida Batool",
+    description: "I’m Nida Full stack devloper,currently working in Pakistan",
+};
+
+const introdata = {
+    title: "I’m Nida Batool",
+    animated: {
+        first: "I love coding",
+        second: "I develop React websites",
+        third: "I develop MERN-Stack Websites",
+    },
+    description: "I create professional websites with a focus on functionality and user experience. Let's build something amazing together!",
+    your_img_url: "nida.jpeg",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am a full-stack web developer with expertise in both frontend and backend technologies. I specialize in React.js for building responsive designs and have strong backend skills using Node.js, Express.js, and MongoDB. I also have experience in UI/UX design for both web and mobile applications.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "Node",
+        value: 80,
+    },
+    {
+        name: "Mongodb",
+        value: 60,
+    },
+    {
+        name: "MERN",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I specialize in UI/UX design for both web and mobile applications. With a keen eye for user-centric design, I create visually appealing and functional interfaces.",
+    },
+    {
+        title: "Frontend",
+        description: "I have expertise in frontend development, focusing on creating responsive websites using React.js. I build interactive user interfaces that adapt seamlessly across devices",
+    },
+    {
+        title: "Backend",
+        description: " I have experience in backend development using Node.js and Express.js for building server-side applications. I also work with MongoDB to manage and store data efficiently.",
+    },
+];
+
+const dataportfolio = [
+     
+    {
+        img: "imga.PNG",
+        description: "Image Search App",
+        link: "https://img-search-swart.vercel.app/",
+    },
+   
+    {
+        img: "imgb.PNG",
+        description: "Create stunning images effortlessly with our innovative image generation app",
+        link: "  https://img-search-2rc7.vercel.app/"
+    },
+    {
+        img: "imgc.PNG",
+        description: "A website showcasing a variety of fresh fruits and vegetables.",
+        link: "https://food-web-nu-three.vercel.app/",
+    },
+
+    {
+        img: "imgd.PNG",
+        description: "TODO list.",
+        link: " https://todo-list2-psi.vercel.app/",
+    },
+    {
+        img: "imge.PNG",
+        description: "FOODFlash is a dynamic restaurant website offering a diverse menu, online reservations, and a seamless dining experience",
+        link: "https://food-flash-omega.vercel.app/",
+    },
+    {
+        img: "imgf.PNG",
+        description: "Portfolio-web",
+        link: " https://portfolio-kappa-cyan-35.vercel.app/",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nida711711@gmail.com",
+    YOUR_FONE: "03100041834",
+    description: "Feel free to reach out for any inquiries or project discussions. I'm here to bring your ideas to life. Let's connect and create something great together",
+   
+    YOUR_SERVICE_ID: "service_38w4ox8",
+    YOUR_TEMPLATE_ID: "template_kteuw2x",
+    YOUR_USER_ID: "TI60_DAjjXZ5CCnSz",
+};
+
+const socialprofils = {
+    github: "https://github.com/NidaKhan711",
+    facebook: "https://www.facebook.com/profile.php?id=61550965312292",
+    linkedin: "https://www.linkedin.com/in/nida-batool-6a5599312?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    Instagram: "https://www.instagram.com/_nidabatool/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
